b1f5c85e12927130706e4d8f3d8533af76aeb464
Brent Ritchie <ritchie.brent@gmail.com>
2022-10-11 20:25:51 -0400

    Add project files.

---
+++ CMP-1005-Fall-2022-Cors/Program.cs
@@ -0,0 +1,44 @@
+namespace CMP_1005_Fall_2022_Cors
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string CorsDefaultPolicyName = "_corsDefaultPolicy";
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddCors(options => {
+                options.AddDefaultPolicy(
+                    policy => {
+                        policy.WithOrigins("https://localhost:7151");
+                    });
+            });
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseCors();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
